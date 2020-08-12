86ad276a84acaa0a7102928afd17c58957187ea8
KaveeshaW <47799209+KaveeshaW@users.noreply.github.com>
2020-08-12 10:49:32 -0500

    might be right

--- src/main/java/com/codedifferently/RemoveCharFromString.java
+++ src/main/java/com/codedifferently/RemoveCharFromString.java
@@ -7,7 +7,11 @@ class Main {
     logger.info(remove("abc", 'a'));
     logger.info(remove("abcd", 'c'));
     logger.info(remove("", 'd'));
-    logger.info(remove("abcd", 'd'));
+    logger.info(removeRecursive("abcdefgh", 'a'));
+    logger.info(removeRecursive("abcd", 'c'));
+    logger.info(removeRecursive("abcd", 'd'));
+    logger.info(remove("aaaaaaaaaaaaaa", 'a'));
+    logger.info(remove("aaaaaaaaaaaaaab", 'a'));
   }
 
   public static String remove(String word, char unwanted){
@@ -21,11 +25,11 @@ class Main {
   }
 
   /*Extra Credit Flex*/
-  public String removeRecursive(String word, char ch){
+  public static String removeRecursive(String word, char ch){
     int index = word.indexOf(ch);
     if(index == -1) {
       return word;
     }
-    return removeRecursive(word.substring(0, index) + word.substring(index), ch);
+    return removeRecursive(word.substring(0, index) + word.substring(index + 1), ch);
   }
 }

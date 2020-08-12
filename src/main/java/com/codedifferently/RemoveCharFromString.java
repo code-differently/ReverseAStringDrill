e39d56100c228c3b72700edc4dbb712ad475708a
Brent Oliver <balexanderoliver@gmail.com>
2020-08-12 13:20:35 -0400

    Completed

--- src/main/java/com/codedifferently/RemoveCharFromString.java
+++ src/main/java/com/codedifferently/RemoveCharFromString.java
@@ -1,17 +1,28 @@
 package com.codedifferently;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
+//import com.sun.org.slf4j.internal.Logger;
+//import com.sun.org.slf4j.internal.LoggerFactory;
 
 public class RemoveCharFromString {
-    private static final Logger logger = LoggerFactory.getLogger(RemoveCharFromString.class);
+    //private static final Logger logger = LoggerFactory.getLogger(RemoveCharFromString.class);
 
     public String remove(String word, char unwanted){
-        return null;
+        return word.replaceAll(unwanted + "", "");
     }
 
     /*Extra Credit Flex*/
     public String removeRecursive(String word, char ch){
-        return null;
+        int chIndex = word.indexOf(ch);
+        if(chIndex == -1)
+            return word;
+
+        StringBuilder ret = new StringBuilder();
+        for(int i = 0; i < word.length(); i++) {
+            if(i != chIndex) {
+                ret.append(word.charAt(i));
+            }
+        }
+
+        return removeRecursive(ret.toString(), ch);
     }
 }

4d75ec8e7fc5284fa8a2c808fef3a6e4bc4b1e06
Faith Wilkins El <fwilkins@terpmail.umd.edu>
2020-08-12 11:54:34 -0400

    Didn't test it yet

--- src/main/java/com/codedifferently/RemoveCharFromString.java
+++ src/main/java/com/codedifferently/RemoveCharFromString.java
@@ -4,14 +4,27 @@ import com.sun.org.slf4j.internal.Logger;
 import com.sun.org.slf4j.internal.LoggerFactory;
 
 public class RemoveCharFromString {
-    private static final Logger logger = LoggerFactory.getLogger(RemoveCharFromString.class);
+    //private static final Logger logger = LoggerFactory.getLogger(RemoveCharFromString.class);
 
     public String remove(String word, char unwanted){
-        return null;
+
+        for (char chars : word.toCharArray()){
+            if(chars == unwanted){
+                word = word.replace(""+chars, "");
+            }
+        }
+        return word;
     }
 
     /*Extra Credit Flex*/
     public String removeRecursive(String word, char ch){
-        return null;
+        if(word.length() == 0){
+            return word;
+        }
+        if(word.charAt(0) == ch){
+            return removeRecursive(word.substring(1), ch);
+        } else {
+            return word.charAt(0) + removeRecursive(word.substring((1)), ch);
+        }
     }
 }

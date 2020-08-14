0ba61d35da8a3de367734db84451e5e04db75be8
Malon Holmes <itsmalonholmes@gmail.com>
2020-08-13 23:30:49 -0400

    Did Recursion

--- src/main/java/com/codedifferently/RemoveCharFromString.java
+++ src/main/java/com/codedifferently/RemoveCharFromString.java
@@ -1,17 +1,35 @@
 package com.codedifferently;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
+//import com.sun.org.slf4j.internal.Logger;
+//import com.sun.org.slf4j.internal.LoggerFactory;
 
 public class RemoveCharFromString {
-    private static final Logger logger = LoggerFactory.getLogger(RemoveCharFromString.class);
+    //private static final Logger logger = LoggerFactory.getLogger(RemoveCharFromString.class);
 
     public String remove(String word, char unwanted){
-        return null;
-    }
+        // Create empty string to return
+        String result = "";
+        // iter through each character in string input, word
+        for (int idx = 0; idx <= word.length() - 1; idx++) {
+            Character currChar = word.charAt(idx);
+            if (!currChar.equals(unwanted)){ // as long as the current character is NOT equal to unwanted, then add it to result
+                result += currChar;
+            }
+        }
+        return result;
+    } // Try with StringBuilder
 
     /*Extra Credit Flex*/
-    public String removeRecursive(String word, char ch){
-        return null;
+    public String removeRecursive(String word, char ch)
+    {
+        int index = word.indexOf(ch);
+        if(index < 0)
+        {
+            return word;
+        }
+        else
+        {
+            return removeRecursive(word.substring(0, index) + word.substring(index + 1), ch);
+        }
     }
 }
